1e6317ee964d7c056fe75f7d395e123784f2836d
Chris Hintz <chrishintz42@gmail.com>
2016-07-14 16:34:27 -0700

    Add Product List Component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,19 @@
+// import what we need from any 3rd party library, our modules or angular
 import { Component } from 'angular2/core';
+import { ProductListComponent } from './products/product-list.component';
 
 @Component({
+  // decorator
     selector: 'pm-app',
     template:`
     <div><h1>{{pageTitle}}</h1>
-      <div>My First Component </div>
+      <pm-products></pm-products>
     </div>
-    `
+    ` ,
+    directives: [ProductListComponent]
 })
+
+// Create a class with code to support the view
 export class AppComponent {
     pageTitle: string = 'Acme Product Management';
 }
